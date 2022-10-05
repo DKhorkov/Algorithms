11861db2c5194dd10733b084a01302644b58e32a
DKhorkov <dkhorkov.dev@gmail.com>
2022-10-05 18:53:38 +0300

    Learned about sorted-lists and arrays, how they work with memory, repeated selection_sort

---
+++ Grokking_Algorithms/Chapter_2/selection_search.py
@@ -0,0 +1,14 @@
+def selection_search(lst):
+    """Алгоритм сортировки выборкой, скорость работы алгоритма О(n ** 2)"""
+    for i in range(len(lst)):
+        lowest = i
+        for j in range(i + 1, len(lst)):
+            if lst[lowest] > lst[j]:
+                lowest = j
+        lst[lowest], lst[i] = lst[i], lst[lowest]
+
+
+a = [5, 3, 6, 2, 7, 1, 0, 15, 22, 4, 11]
+print(a)
+selection_search(a)
+print(a)
