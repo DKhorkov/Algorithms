b9a06c8c4210cefd90fcf94b3f807d6d34616847
DKhorkov <dkhorkov.dev@gmail.com>
2022-11-04 13:59:03 +0300

    Made algorithm working as conceived and wrote doc-strings

--- Grokking_Algorithms/Chapter_9_dynamic_programming/configs.py
+++ Grokking_Algorithms/Chapter_9_dynamic_programming/configs.py
@@ -5,5 +5,11 @@ guitar = {'name': 'guitar', 'weight': 1, 'amount': 1500}
 laptop = {'name': 'laptop', 'weight': 3, 'amount': 2000}
 recorder = {'name': 'recorder', 'weight': 4, 'amount': 3000}
 iphone = {'name': 'iphone', 'weight': 1, 'amount': 1600}
+flower = {'name': 'flower', 'weight': 1, 'amount': 800}
+book = {'name': 'book', 'weight': 1, 'amount': 200}
+watches = {'name': 'watches', 'weight': 2, 'amount': 3100}
+gun = {'name': 'gun', 'weight': 5, 'amount': 2500}
+food = {'name': 'food', 'weight': 6, 'amount': 6000}
 
-items = [iphone, guitar, recorder, laptop]
+# Need to put all items in list for correct work:
+items = [iphone, guitar, recorder, laptop, flower, book, watches, gun, food]
--- Grokking_Algorithms/Chapter_9_dynamic_programming/dynamic_programming_example.py
+++ Grokking_Algorithms/Chapter_9_dynamic_programming/dynamic_programming_example.py
@@ -7,35 +7,38 @@ class DynamicProgramming:
 
     def __init__(self, size):
         self.package_size = size
-        self.alternative = {}
         self.items = items
         self.max = {}
 
-    def create_max_keys_and_alternative(self):
+    def create_max_keys(self) -> None:
+        """Function creates template of max amounts dict for every weight number in range of package size, which was
+        given in init."""
         size = self.package_size
         for i in range(self.package_size):
             self.max[size] = 0
-            self.alternative[size] = {'name': None, 'amount': 0}
             size -= 1
 
-    def get_alternative(self, item, current_weight):
+    def get_alternative(self, item: dict, current_weight: int) -> dict:
+        """Function takes 3 mandatory params and calculates the best alternative for given weight number.
+        Returns alternative (dict)."""
         if current_weight > 0 and item['name'] not in self.max[current_weight]['name']:
             alternative = {'name': f"{self.max[current_weight]['name']} and " + item['name'],
                            'amount': self.max[current_weight]['amount'] + item['amount']}
-        elif current_weight > 0 and item['name'] not in self.alternative[current_weight]['name']:
-            alternative = {'name': f"{self.alternative[current_weight]['name']} and " + item['name'],
-                           'amount': self.alternative[current_weight]['amount'] + item['amount']}
         else:
-            alternative = {'name': None, 'amount': 0}
+            try:
+                alternative = {'name': self.max[current_weight]['name'],
+                               'amount': self.max[current_weight]['amount']}
+            except KeyError:
+                alternative = {'name': None, 'amount': 0}
         return alternative
 
-    def main_comparison(self, item, weight, current_weight):
+    def main_comparison(self, item: dict, weight: int, current_weight: int) -> None:
+        """Main logic function of dynamic algorithm. Takes 3 mandatory params and compares amount of item with the
+        alternative amount to check, which is greater. Then the greatest amount will be putted in max dict."""
         try:
             current_weight -= item['weight']
             alternative = self.get_alternative(item, current_weight)
             if self.max[weight]['amount'] < item['amount']:
-                self.alternative[weight] = {'name': self.max[weight]['name'],
-                                            'amount': self.max[weight]['amount']}
                 self.max[weight] = {'name': item['name'], 'amount': item['amount']}
                 if current_weight > 0 and self.max[current_weight]['name'] != item['name']:
                     self.max[weight]['amount'] += self.max[current_weight]['amount']
@@ -51,17 +54,20 @@ class DynamicProgramming:
                     self.max[weight] = {'name': item['name'], 'amount': item['amount']}
 
     def main(self):
-        self.create_max_keys_and_alternative()
+        """Main function of dynamic algorithm. Iterates through items and fill max dict. As the result, prints a string,
+        in which informs user, what will be the best pack to fill a backpack with the predetermined weight and the
+        amount of calculated pack."""
+        self.create_max_keys()
         for item in self.items:
             for weight in sorted(self.max):
                 current_weight = weight
                 if weight >= item['weight']:
                     self.main_comparison(item, weight, current_weight)
+        print(f"To fill a backpack with a volume of {self.package_size} kilos, it is best to put "
+              f"{self.max[self.package_size]['name']} in it to achieve the maximum cost, which will be "
+              f"{self.max[self.package_size]['amount']}$!")
 
 
 if __name__ == '__main__':
-    dp = DynamicProgramming(5)
+    dp = DynamicProgramming(10)
     dp.main()
-    print(dp.max)
-
-
