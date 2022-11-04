02b43b013601d2fb64649b4907786df82cc94916
DKhorkov <dkhorkov.dev@gmail.com>
2022-11-04 12:55:20 +0300

    Refactored algorithm

---
+++ Grokking_Algorithms/Chapter_9_dynamic_programming/configs.py
@@ -0,0 +1,9 @@
+"""Configs of items for dynamic programming."""
+
+
+guitar = {'name': 'guitar', 'weight': 1, 'amount': 1500}
+laptop = {'name': 'laptop', 'weight': 3, 'amount': 2000}
+recorder = {'name': 'recorder', 'weight': 4, 'amount': 3000}
+iphone = {'name': 'iphone', 'weight': 1, 'amount': 1600}
+
+items = [iphone, guitar, recorder, laptop]
--- Grokking_Algorithms/Chapter_9_dynamic_programming/dynamic_programming_example.py
+++ Grokking_Algorithms/Chapter_9_dynamic_programming/dynamic_programming_example.py
@@ -1,17 +1,14 @@
 """Задача с помощью динамического программирования найти оптимальный набор вещей, которые могут поместитсья в рюкзак
 заданного размера на примере задачи из книги."""
+from configs import items
 
 
 class DynamicProgramming:
 
     def __init__(self, size):
         self.package_size = size
-        self.guitar = {'name': 'guitar', 'weight': 1, 'amount': 1500}
-        self.laptop = {'name': 'laptop', 'weight': 3, 'amount': 2000}
-        self.recorder = {'name': 'recorder', 'weight': 4, 'amount': 3000}
-        self.iphone = {'name': 'iphone', 'weight': 1, 'amount': 1600}
         self.alternative = {}
-        self.items = [self.iphone, self.guitar, self.recorder, self.laptop]
+        self.items = items
         self.max = {}
 
     def create_max_keys_and_alternative(self):
@@ -21,38 +18,45 @@ class DynamicProgramming:
             self.alternative[size] = {'name': None, 'amount': 0}
             size -= 1
 
+    def get_alternative(self, item, current_weight):
+        if current_weight > 0 and item['name'] not in self.max[current_weight]['name']:
+            alternative = {'name': f"{self.max[current_weight]['name']} and " + item['name'],
+                           'amount': self.max[current_weight]['amount'] + item['amount']}
+        elif current_weight > 0 and item['name'] not in self.alternative[current_weight]['name']:
+            alternative = {'name': f"{self.alternative[current_weight]['name']} and " + item['name'],
+                           'amount': self.alternative[current_weight]['amount'] + item['amount']}
+        else:
+            alternative = {'name': None, 'amount': 0}
+        return alternative
+
+    def main_comparison(self, item, weight, current_weight):
+        try:
+            current_weight -= item['weight']
+            alternative = self.get_alternative(item, current_weight)
+            if self.max[weight]['amount'] < item['amount']:
+                self.alternative[weight] = {'name': self.max[weight]['name'],
+                                            'amount': self.max[weight]['amount']}
+                self.max[weight] = {'name': item['name'], 'amount': item['amount']}
+                if current_weight > 0 and self.max[current_weight]['name'] != item['name']:
+                    self.max[weight]['amount'] += self.max[current_weight]['amount']
+                    self.max[weight]['name'] += f" and {self.max[current_weight]['name']}"
+            elif self.max[weight]['amount'] < alternative['amount']:
+                self.max[weight] = {'name': alternative['name'], 'amount': alternative['amount']}
+        except TypeError:
+            try:
+                if self.max[weight]['amount'] < item['amount']:
+                    self.max[weight] = {'name': item['name'], 'amount': item['amount']}
+            except TypeError:
+                if self.max[weight] < item['amount']:
+                    self.max[weight] = {'name': item['name'], 'amount': item['amount']}
+
     def main(self):
         self.create_max_keys_and_alternative()
         for item in self.items:
             for weight in sorted(self.max):
                 current_weight = weight
                 if weight >= item['weight']:
-                    try:
-                        current_weight -= item['weight']
-                        if current_weight > 0 and item['name'] not in self.max[current_weight]['name']:
-                            alternative = {'name': f"{self.max[current_weight]['name']} and " + item['name'],
-                                       'amount': self.max[current_weight]['amount'] + item['amount']}
-                        elif current_weight > 0 and item['name'] not in self.alternative[current_weight]['name']:
-                            alternative = {'name': f"{self.alternative[current_weight]['name']} and " + item['name'],
-                                           'amount': self.alternative[current_weight]['amount'] + item['amount']}
-                        else:
-                            alternative = {'name': None, 'amount': 0}
-                        if self.max[weight]['amount'] < item['amount']:
-                            self.alternative[weight] = {'name': self.max[weight]['name'],
-                                                        'amount': self.max[weight]['amount']}
-                            self.max[weight] = {'name': item['name'], 'amount': item['amount']}
-                            if current_weight > 0 and self.max[current_weight]['name'] != item['name']:
-                                self.max[weight]['amount'] += self.max[current_weight]['amount']
-                                self.max[weight]['name'] += f" and {self.max[current_weight]['name']}"
-                        elif self.max[weight]['amount'] < alternative['amount']:
-                            self.max[weight] = {'name': alternative['name'], 'amount': alternative['amount']}
-                    except TypeError:
-                        try:
-                            if self.max[weight]['amount'] < item['amount']:
-                                self.max[weight] = {'name': item['name'], 'amount': item['amount']}
-                        except TypeError:
-                            if self.max[weight] < item['amount']:
-                                self.max[weight] = {'name': item['name'], 'amount': item['amount']}
+                    self.main_comparison(item, weight, current_weight)
 
 
 if __name__ == '__main__':
