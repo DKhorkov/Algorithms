cf0db6b54e053d5600688ef256918b5af327e2bb
DKhorkov <dkhorkov.dev@gmail.com>
2024-04-16 18:26:18 +0300

    Learned randomized selection search, which is based on quick-sort algorithm

---
+++ Algorithms_Construction_and_Analysis/Chapter_9_sorting_and_ordinal_statistics/min_and_max_simultaneous_search.py
@@ -0,0 +1,58 @@
+"""
+Данный алгоритм полезен для одновременного поиска минимального и максимального элемента во множестве.
+Также делается предположение, что множетсво состоит из уникальных неповторяющихся элементов.
+
+Асимптоматическая скорость алгоритма O(n/2) - поскольку мы сравниваем пары = O(n).
+Количество сравнений равно "пол" от 3n/2, вместо 2n - 2, если бы мы сравнивали каждый элемент с минимумом и максимумом.
+"""
+
+from typing import List, Tuple, Union
+
+
+class MinAndMaxSimultaneousSearch:
+
+    def __init__(self, arr: List[int]) -> None:
+        self._arr: List[int] = arr
+        self._min_value: Union[int, float] = float('-inf')
+        self._max_value: Union[int, float] = float('inf')
+        self._index: int = 0
+
+        self._prepare()
+
+    def _prepare(self) -> None:
+        if len(self._arr) % 2 == 1:
+            self._index = 1
+            self._min_value = self._arr[0]
+            self._max_value = self._arr[0]
+
+    def search(self) -> Tuple[int, int]:
+        while self._index < len(self._arr):
+            first_number = self._arr[self._index]
+            second_number = self._arr[self._index + 1]
+
+            # Первое сравнение элементов между собой для определения, какой из них меньше
+            if first_number < second_number:
+                current_min = first_number
+                current_max = second_number
+            else:
+                current_min = second_number
+                current_max = first_number
+
+            # Второе сравнение текущего меньшего с наименьшим
+            if current_min < self._min_value:
+                self._min_value = current_min
+
+            # Третье сравнение текущего большего с наибольшим
+            if current_max > self._max_value:
+                self._max_value = current_max
+
+            # Переход к следующей паре для сравнения
+            self._index += 2
+
+        return self._min_value, self._max_value
+
+
+if __name__ == '__main__':
+    array: List[int] = [1, 15, 4, 425, 3, 1324, 2, 7, 90]
+    minimum, maximum = MinAndMaxSimultaneousSearch(arr=array).search()
+    print(minimum, maximum)
---
+++ Algorithms_Construction_and_Analysis/Chapter_9_sorting_and_ordinal_statistics/randomized_selection.py
@@ -0,0 +1,69 @@
+"""
+Randomized Selection - алгоритм основанный на быстрой сортировке, но использующийся для выбора k-th
+(в данном случае наименьшего) элемента во входном массиве. Например, если k = 3, то мы ищем 3 наименьший
+элемент во входном массиве. В отличие от алгоритма быстрой сортировки, рекурсивно будет вызываться поиск только для
+одной из двух частей разделенного по опорному элементу входного массива.
+
+В алгоритм НЕ ДОЛЖЕН передаваться пустой массив, а k должно принадлежать диапазону [1, len(arr)].
+
+В наихудшем случае асивптоматическая скорость работы алгоритма составит O(n ^ 2), поскольку рандом не исключает
+выбор несбалансированного (плохого) опорного элемента для каждого рекурсивного вызова. Однако ожидаемое время работы
+алгоритма будет O(n), поскольку он рандомизирован и никакие входные данные не могут гарантированно привести к
+наихудшему поведению алгоритма.
+"""
+
+from typing import List, Optional
+
+from Algorithms_Construction_and_Analysis.Chapter_7_quick_sort.randomized_quick_sort import RandomizedQuickSort
+
+
+class RandomizedSelection(RandomizedQuickSort):
+
+    def __init__(self, arr: List[int], k: int) -> None:
+        super().__init__(arr=arr)
+        self._k: int = k
+
+        if not self._arr:
+            raise ValueError("arr cannot be empty")
+
+        if self._k < 1 or self._k > len(self._arr):
+            raise ValueError("k cannot be greater than len(arr) or less than 1")
+
+    def search(self, low_index: int = 0, high_index: Optional[int] = None) -> int:
+        if high_index is None:
+            high_index = len(self._arr) - 1
+
+        """
+        Базовый выход из рекурсии, в случае, когда массив состоит из 1 элемента, который и будет возвращен.
+        """
+        if low_index == high_index:
+            return self._arr[low_index]
+
+        """
+        В ходе выбора опорного элемента производится его сортировка так, что все эелементы во входном массиве до 
+        опорного элемента будут меньше или равны ему, а все элементы после входного массива будут больше 
+        опорного элемента.
+        
+        Далее вычисляется количество элементов в подмассиве меньшем подмассиве, то есть количество элементов, 
+        попадающих в меньшую часть разбиения + опорный элемент. 
+        Если количество элементов в выбранном подмассиве равняется k-th, то ответом является опорный элемент,
+        поскольку подмассив уже будет отсортирован ранее.
+        
+        В ином случае, вызывается рекурсия.
+        """
+        pivot_index: int = self._get_randomized_pivot_index(low_index=low_index, high_index=high_index)
+        elements_count: int = pivot_index - low_index + 1
+        if elements_count == self._k:
+            return self._arr[pivot_index]
+        elif self._k < elements_count:
+            return self.search(low_index=low_index, high_index=pivot_index - 1)
+        else:
+            self._k -= elements_count
+            return self.search(low_index=pivot_index + 1, high_index=high_index)
+
+
+if __name__ == '__main__':
+    array: List[int] = [3, 1, 8, 4, 7, 9]
+    for i in range(100):
+        k_minimum: int = RandomizedSelection(arr=array, k=1).search()
+        print(k_minimum, end='')
