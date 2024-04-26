b40f6d07b4a8d548a2b82af89ba4a6bc2cb1a4b2
DKhorkov <dkhorkov.dev@gmail.com>
2024-04-27 00:13:04 +0300

    Finished implementing binary search tree

--- Algorithms_Construction_and_Analysis/Chapter_12_binary_search_tree/binary_search_tree.py
+++ Algorithms_Construction_and_Analysis/Chapter_12_binary_search_tree/binary_search_tree.py
@@ -11,12 +11,15 @@ from typing import Optional, List, Any
 
 class Node:
 
-    def __init__(self, key: int) -> None:
+    def __init__(self, key: int, value: Any = None) -> None:
         self.parent: Optional[Node] = None
         self.left: Optional[Node] = None
         self.key: int = key
-        self.value: Any
+        self.value: Any = value
         self.right: Optional[Node] = None
+
+    def __repr__(self) -> str:
+        return f"Node(key={self.key}, value={self.value})"
 
 
 class BinarySearchTree:
@@ -24,21 +27,21 @@ class BinarySearchTree:
     def __init__(self, root: Optional[Node] = None) -> None:
         self.root: Optional[Node] = root
 
-    def traverse(self, node: Node = None) -> List[Node]:
+    def traverse(self, node: Node = 'root') -> List[Node]:
         """
         Обходим дерево так, чтобы список узлов вернулся в отсортированном по ключам порядке,
         что происходит за счет концепции бинарного дерева поиска.
         """
 
-        if node is None:
+        if node == 'root':
             node: Optional[Node] = self.root
 
         if node:
-            return self.traverse(node.left) + [node.key] + self.traverse(node.right)
+            return self.traverse(node=node.left) + [node] + self.traverse(node=node.right)
 
         return []
 
-    def search(self, key: int, node: Node = None) -> Optional[Node]:
+    def search(self, key: int, node: Node = 'root') -> Optional[Node]:
         """
         Если дерево пустое или узла с искомым ключом в дереве нет, то получим NoneType.
         Если искомый ключ меньше ключа родительского узла, то по определению бинарного дерева он может
@@ -46,18 +49,18 @@ class BinarySearchTree:
         Если искомый ключ равен ключу узла, значит это и есть искомый узел.
         """
 
-        if node is None:
+        if node == 'root':
             node: Optional[Node] = self.root
 
         if not node or node.key == key:
             return node
 
         if node.key < key:
-            return self.search(key, node.left)
+            return self.search(key=key, node=node.right)
         else:
-            return self.search(key, node.right)
+            return self.search(key=key, node=node.left)
 
-    def find_max(self, node: Optional[Node] = None) -> Optional[Node]:
+    def find_max(self, node: Optional[Node] = 'root') -> Optional[Node]:
         """
         Является выполнением задачи 12.2.2.
 
@@ -65,15 +68,15 @@ class BinarySearchTree:
         В ином случае максимум находится в самом правом узле наследнике на последнем уровне бинарного дерева.
         """
 
-        if node is None:
+        if node == 'root':
             node: Optional[Node] = self.root
 
         if not node or not node.right:
             return node
 
-        return self.find_max(node.right)
+        return self.find_max(node=node.right)
 
-    def find_min(self, node: Optional[Node] = None) -> Optional[Node]:
+    def find_min(self, node: Optional[Node] = 'root') -> Optional[Node]:
         """
         Является выполнением задачи 12.2.2.
 
@@ -81,15 +84,15 @@ class BinarySearchTree:
         В ином случае минимум находится в самом левом узле наследнике на последнем уровне бинарного дерева.
         """
 
-        if node is None:
+        if node == 'root':
             node: Optional[Node] = self.root
 
         if not node or not node.left:
             return node
 
-        return self.find_min(node.left)
+        return self.find_min(node=node.left)
 
-    def successor(self, node: Optional[Node] = None) -> Optional[Node]:
+    def successor(self, node: Optional[Node] = 'root') -> Optional[Node]:
         """
         Данный метод ищет ближайший больший узел к заданному
         (как в случае, когда все узлы были бы отсортированы методом self.traverse()).
@@ -104,11 +107,11 @@ class BinarySearchTree:
         а нам нужен узел, который большего текущего.
         """
 
-        if node is None:
+        if node == 'root':
             node: Optional[Node] = self.root
 
         if node.right:
-            return self.find_min(node.right)
+            return self.find_min(node=node.right)
 
         successor_node: Optional[Node] = node.parent
         while successor_node and node == successor_node.right:
@@ -117,7 +120,7 @@ class BinarySearchTree:
 
         return successor_node
 
-    def predecessor(self, node: Optional[Node] = None) -> Optional[Node]:
+    def predecessor(self, node: Optional[Node] = 'root') -> Optional[Node]:
         """
         Является выполнением задачи 12.2.3.
 
@@ -134,11 +137,11 @@ class BinarySearchTree:
         а нам нужен узел, который меньше текущего.
         """
 
-        if node is None:
+        if node == 'root':
             node: Optional[Node] = self.root
 
         if node.left:
-            return self.find_max(node.left)
+            return self.find_max(node=node.left)
 
         predecessor_node: Optional[Node] = node.parent
         while predecessor_node and node == predecessor_node.left:
@@ -146,3 +149,142 @@ class BinarySearchTree:
             predecessor_node = node.parent
 
         return predecessor_node
+
+    def insert(self, key: int, value: Any = None) -> None:
+        """
+        Создаем новый узел и продвигаемся по дереву вниз до тех пор, пока не найдем пустой дочерний узел,
+        куда должен будет быть вставлен новый узел.
+
+        Временный узел служит как указатель в поисках пустого узла, куда будет помещен новый узел.
+        Далее связываем новый узел и найденного родителя по ключу.
+        Если же родителя нет, то новый узел должен стать корневым в дереве.
+        """
+
+        new_node: Node = Node(key=key, value=value)
+        parent: Optional[Node] = None
+        temp: Optional[Node] = self.root
+        while temp:
+            parent = temp
+            if new_node.key < temp.key:
+                temp = temp.left
+            else:
+                temp = temp.right
+
+        new_node.parent = parent
+
+        if not parent:
+            self.root = new_node
+        elif new_node.key < parent.key:
+            parent.left = new_node
+        else:
+            parent.right = new_node
+
+    def delete(self, key: int) -> None:
+        """
+        Процедура удаления узла из дерева является самой сложной, поскольку здесь имеются 4 вариант развития событий:
+        1) У удаляемого узла нет дочерних узлов, тогда мы просто удаляем его, делая дерево пустым,
+        а его корень NoneType;
+        2) У удаляемого узла есть только правый дочерний узел, тогда мы заменяем удаляемый узел его
+        правым дочерним узлом и связываем правый дочерний узел с родителем удаляемого узла;
+        3) У удаляемого узла есть только левый дочерний узел, тогда мы заменяем удаляемый узел его
+        левым дочерним узлом и связываем левым дочерний узел с родителем удаляемого узла;
+        4) У удаляемого узла есть и левый, и правый дочерние узлы. В таком случае мы ищем в правой ветке удаляемого узла
+        такой узел, у которого не будет левого дочернего узла (поскольку левый дочерний узел меньше удаляемого,
+        а любой узел в правой ветке больше удаляемого, то мы сможем заменить удаляемый узел искомым).
+
+        4.1. Если искомый узел является правым дочерним узлом удаляемого, то мы просто заменяем удаляемый узел его
+        правым дочерним узлом и делаем левый дочерний узел удаляемого узла левым дочерним узлом правого дочернего узла,
+        который теперь заменил удаляемый узел.
+
+        4.2. В ином случае, мы ищем в правой ветке удаляемого узла нужный нам узел. Затем заменяем найденный узел его
+        правым дочерним узлом для сохранения иерархии дерева. После чего заменяем удаляемый узел найденным узлом, и
+        также делаем левый дочерний узел удаляемого узла левым дочерним узлом найденного узла,
+        который теперь заменил удаляемый узел.
+        """
+
+        node_to_delete: Optional[Node] = self.search(key=key)
+        if not node_to_delete:
+            raise KeyError
+
+        # Обработка первого и второго случая. Если node_to_delete.right == None - это первый случай:
+        if not node_to_delete.left:
+            self._transplant(to_delete=node_to_delete, to_transplant=node_to_delete.right)
+
+        # Обработка третьего случая:
+        elif not node_to_delete.right:
+            self._transplant(to_delete=node_to_delete, to_transplant=node_to_delete.left)
+
+        # Обработка 4 случая
+        else:
+            """
+            Поскольку минимум находится в самом левом узле наследнике на последнем уровне бинарного дерева,
+            то мы найдем минимальный узел в правой ветке удаляемого узла. Если у него нет левого дочернего узла, 
+            значит он минимальный в текущей ветке
+            """
+            node_to_transplant: Node = self.find_min(node=node_to_delete.right)
+
+            # Случай 4.2.
+            if node_to_transplant.parent != node_to_delete:
+                # Заменяем найденный узел его правым дочерним узлом для сохранения иерархии дерева
+                self._transplant(to_delete=node_to_transplant, to_transplant=node_to_transplant.right)
+
+                # Делаем правую ветвь удаляемого узла правой ветвью найденного узла.
+                node_to_transplant.right = node_to_delete.right
+                node_to_transplant.right.parent = node_to_transplant
+
+            """
+            Общая часть для пунктов 4.1 и 4.2 - замена удаляемого узла найденным и 
+            привязка левого дочернего узла удаляемого узла к найденному узлу.
+            """
+            self._transplant(to_delete=node_to_delete, to_transplant=node_to_transplant)
+            node_to_transplant.left = node_to_delete.left
+            node_to_delete.left.parent = node_to_transplant
+
+    def _transplant(self, to_delete: Node, to_transplant: Optional[Node]) -> None:
+        """
+        Данный метод заменяет один узел другим и нужен для процедуры удаления узла из дерева.
+
+        Если у узла, который должен быть удален, нет родителя, то новый узел становится корневым,
+        поскольку удаляемый узел был корневым.
+
+        В ином случае, идет проверка, является ли удаляемый узел левым или правым дочерним узлом своего родителя,
+        чтобы новый узел занял его место.
+
+        Если новый узел не является NoneType, то необходимо привязать его к родителю, коим будет являться родитель
+        удаляемого узла.
+        """
+
+        if not to_delete.parent:
+            self.root = to_transplant
+        elif to_delete.parent.left == to_delete:
+            to_delete.parent.left = to_transplant
+        else:
+            to_delete.parent.right = to_transplant
+
+        if to_transplant:
+            to_transplant.parent = to_delete.parent
+
+
+if __name__ == '__main__':
+    values: List[int] = [2, 8, 4, 3, 12, 10, 5, 7, 1, 9]
+    tree: BinarySearchTree = BinarySearchTree()
+
+    for value in values:
+        tree.insert(key=value)
+
+    print(*tree.traverse())
+
+    print(tree.find_min())
+    print(tree.find_max())
+
+    searched: Node = tree.search(key=3)
+    print(searched)
+    successor: Node = tree.successor(node=searched)
+    print(successor)
+    predecessor: Node = tree.predecessor(node=searched)
+    print(predecessor)
+
+    tree.delete(key=1)  # 1 случай
+    print(*tree.traverse())
+    tree.delete(key=2)  # 4 случай
+    print(*tree.traverse())
